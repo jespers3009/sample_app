b7eac8944696761c5c876f78fad791b46487f0ab
jespers <jesper.sondergaard@gmail.com>
2011-08-05 01:57:48 -0700

    User model with Passwords, and user show page

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ group :development do
   	gem 'autotest-fsevent', '0.2.4'
   	gem 'autotest-growl', '0.2.9'
   	gem 'annotate', '2.4.0'
+	gem 'factory_girl_rails', '1.0'
+	gem 'gravatar_image_tag', '1.0.0.pre2'
 end
 
 group :test do
@@ -26,6 +28,8 @@ group :test do
   	gem 'autotest-rails-pure', '4.1.2'
   	gem 'autotest-fsevent', '0.2.4'
   	gem 'autotest-growl', '0.2.9'
+	gem 'factory_girl_rails', '1.0'
+	gem 'gravatar_image_tag', '1.0.0.pre2'
 end
 
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,43 +2,36 @@
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
 require 'spec_helper'
 
 describe User do
-  
-  before (:each) do
-    @attr = {:name =>"Example User", :email => "user@example.com"}
+
+  before(:each) do
+    @attr = {
+      :name => "Example User",
+      :email => "user@example.com",
+      :password => "foobar",
+      :password_confirmation => "foobar"
+    }
   end
-  
-  it "should reject duplicate email addresses" do
-    # put a user with given email address into the database
+
+  it "should create a new instance given valid attributes" do
     User.create!(@attr)
-    user_with_duplicate_email = User.new(@attr)
-    user_with_duplicate_email.should_not be_valid
-  end
-  
-  it "should reject email addresses identical to up to case" do
-    upcased_email = @attr[:email].upcase
-    User.create!(@attr.merge(:email => upcased_email))
-    user_with_duplicate_Upcase_email = User.new(@attr)
-    user_with_duplicate_Upcase_email.should_not be_valid
   end
     
-    it "should create a new instance given valid attributtes" do
-      User.create(@attr)
-    end
-    
-    it "should require a name" do
+  it "should require a name" do
     no_name_user = User.new(@attr.merge(:name => ""))
     no_name_user.should_not be_valid
-    end
+  end
     
     it "shoudl require an email address" do
       no_email_user = User.new(@attr.merge(:email=> ""))
@@ -66,5 +59,84 @@ describe User do
         invalid_email_user.should_not be_valid
       end
     end
+    
+    it "should reject duplicate email addresses" do
+      # put a user with given email address into the database
+      User.create!(@attr)
+      user_with_duplicate_email = User.new(@attr)
+      user_with_duplicate_email.should_not be_valid
+    end
+
+    it "should reject email addresses identical to up to case" do
+      upcased_email = @attr[:email].upcase
+      User.create!(@attr.merge(:email => upcased_email))
+      user_with_duplicate_Upcase_email = User.new(@attr)
+      user_with_duplicate_Upcase_email.should_not be_valid
+    end
+    
+    describe "password validations" do
+      
+      it "should require a password" do
+        User.new(@attr.merge(:password => "", :password_confirmation => "")).
+          should_not be_valid
+        end
+        
+        it "should require a matching password confirmation" do
+          User.new(@attr.merge(:password_confirmation => "invalid")).
+            should_not be_valid
+          end
+          
+          it "should reject short passwords" do
+            short = "a" * 5
+            hash = @attr.merge(:password => short, :password_confirmation => short)
+            User.new(hash).should_not be_valid
+          end
+          
+          it "should reject long passwords" do
+            long = "a" * 41
+            hash = @attr.merge(:password => long, :password_confirmation => long)
+            User.new(hash).should_not be_valid
+          end
+        end
+        
+        describe "password encryption" do
+          before (:each) do
+            @user = User.create!(@attr)
+          end
+          
+          it "should have an encrypted password attribute" do
+            @user.should respond_to(:encrypted_password)
+          end
+          
+          it "should set the encrypted password" do
+            @user.encrypted_password.should_not be_blank
+          end
+          
+          it "should be true if the passwords match" do
+            @user.has_password?(@attr[:password]).should be_true
+          end
+          
+          it "should be false if the passwords don't match" do
+            @user.has_password?("invalid").should be_false
+          end
+        end
+        
+        describe "authenticated method" do
+          
+          it "should return nil on email/password mismatch" do
+            wrong_password_user = User.authenticate(@attr[:email],"wrongpass")
+            wrong_password_user.should be_nil
+          end
+          
+          it "should return nil for an email address with no user" do
+            nonexistent_user = User.authenticate("bar@foo.com",@attr[:password])
+            nonexistent_user.should be_nil
+          end
+          
+          it "should return the user on email/password match" do
+            matching_user = User.authenticate(@attr[:email], @attr[:password])
+            matching_user.should == @user
+          end
+        end 
 end
 
