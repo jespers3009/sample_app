855589629d3c8bb6b6b3a193ba4aac22f684d3e3
jespers <jesper.sondergaard@gmail.com>
2011-08-08 09:30:44 -0700

    fix for homepage

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ SampleApp::Application.routes.draw do
   match '/signup',  :to => 'users#new'
   
   get "pages/home"
+  root :to => 'pages#home'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
@@ -58,7 +59,7 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "pages#home"
+
 
   # See how all your routes lay out with "rake routes"
 
